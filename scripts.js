dd9f368a4c8e68578aae590ca00b6ea96ccd0daf
dsantamaria89 <75281950+dsantamaria89@users.noreply.github.com>
2021-06-07 11:50:12 -0700

    Edited JS File
    Added userplay function, computerPlay function.

--- scripts.js
+++ scripts.js
@@ -8,24 +8,82 @@ User will play Rock Paper Scissors against computer opponent.
 
 ----------------------------------------------------------------
 
-Game Algorithm
---------------
-1. Create a variable to hold user input
-2. Create a variable to hold computer choice
-3. create a variable to hold random number
-4. create a variable to hold number of turns and initialize with 5
-5. create a variable to hold user score
-6. create a variable to hold computer score
-7. Begin loop for number of turns. 
-8. Ask user to enter "Rock" , "Paper", or "Scissors" 
-9. if the user inputs anything other than "rock", "paper", or "scissors", 
-   continue asking user to enter choice
-10. Generate Random number between 1-3.
-11. if number is 1 computer choice is "rock", 
-    else if number is 2 computer choice is "paper",
-    else if number is 3 computer choice is "scissors"
-
-
-    FIGURE OUT THE REST LATER.........
-
 */
+
+//* user and computer choices
+let userChoice;
+let computerChoice;
+
+//ComputerPlay Function.
+function computerPlay() {
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  switch (randomNumber) {
+    case 1:
+      return "ROCK";
+      break;
+    case 2:
+      return "PAPER";
+      break;
+    case 3:
+      return "SCISSORS";
+  }
+}
+
+//userPlay Function
+function userPlay() {
+  let keepAsking = true;
+
+  while (keepAsking) {
+    userChoice = prompt("Enter Rock, Paper or Scissors");
+    userChoice = userChoice.toUpperCase();
+    if (
+      userChoice !== "ROCK" &&
+      userChoice !== "PAPER" &&
+      userChoice !== "SCISSORS"
+    ) {
+      keepAsking = true;
+      alert("You must enter either rock, paper, or scissors!");
+    } else {
+      keepAsking = false;
+    }
+  }
+  return userChoice;
+}
+
+//playRound Function
+function playRound(user, computer) {
+  let keepPlaying = true;
+  let message;
+
+  while (keepPlaying) {
+    if (user === "ROCK" && computer === "SCISSORS") {
+      message = "You win!";
+      keepPlaying = false;
+    } else if (user === "ROCK" && computer === "PAPER") {
+      message = "You lose!";
+      keepPlaying = false;
+    } else if (user === "PAPER" && computer === "SCISSORS") {
+      message = "You lose!";
+      keepPlaying = false;
+    } else if (user === "PAPER" && computer === "ROCK") {
+      message = "You win!";
+      keepPlaying = false;
+    } else if (user === "SCISSORS" && computer === "ROCK") {
+      message = "You Lose!";
+      keepPlaying = false;
+    } else if (user === "SCISSORS" && computer === "PAPER") {
+      message = "You Win!";
+      keepPlaying = false;
+    }
+  }
+
+  return message;
+}
+
+userChoice = userPlay();
+console.log(userChoice);
+computerChoice = computerPlay();
+
+let result = playRound(userChoice, computerChoice);
+
+console.log(result);
