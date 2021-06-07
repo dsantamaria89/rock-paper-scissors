78bd18936afb2bfb333535edae26f58beafaf4b3
dsantamaria89 <75281950+dsantamaria89@users.noreply.github.com>
2021-06-06 21:20:47 -0700

    Added JS file

---
+++ scripts.js
@@ -0,0 +1,31 @@
+/*
+The Odin Project
+---------------------------------------------------------------
+
+Rock Paper Scissors Game 
+
+User will play Rock Paper Scissors against computer opponent.
+
+----------------------------------------------------------------
+
+Game Algorithm
+--------------
+1. Create a variable to hold user input
+2. Create a variable to hold computer choice
+3. create a variable to hold random number
+4. create a variable to hold number of turns and initialize with 5
+5. create a variable to hold user score
+6. create a variable to hold computer score
+7. Begin loop for number of turns. 
+8. Ask user to enter "Rock" , "Paper", or "Scissors" 
+9. if the user inputs anything other than "rock", "paper", or "scissors", 
+   continue asking user to enter choice
+10. Generate Random number between 1-3.
+11. if number is 1 computer choice is "rock", 
+    else if number is 2 computer choice is "paper",
+    else if number is 3 computer choice is "scissors"
+
+
+    FIGURE OUT THE REST LATER.........
+
+*/
