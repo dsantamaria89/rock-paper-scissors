d4b10813331f6c76f9fc9cc690d9e14dc0a08507
dsantamaria89 <75281950+dsantamaria89@users.noreply.github.com>
2021-06-07 17:14:49 -0700

    JS File Completed
    JS logic completed.

--- scripts.js
+++ scripts.js
@@ -10,11 +10,15 @@ User will play Rock Paper Scissors against computer opponent.
 
 */
 
-//* user and computer choices
+//* variables
 let userChoice;
 let computerChoice;
+let result;
+let userScore = 0;
+let computerScore = 0;
+let turns = 5;
 
-//ComputerPlay Function.
+//*ComputerPlay Function.
 function computerPlay() {
   let randomNumber = Math.floor(Math.random() * 3 + 1);
   switch (randomNumber) {
@@ -29,7 +33,7 @@ function computerPlay() {
   }
 }
 
-//userPlay Function
+//*userPlay Function
 function userPlay() {
   let keepAsking = true;
 
@@ -55,35 +59,48 @@ function playRound(user, computer) {
   let keepPlaying = true;
   let message;
 
-  while (keepPlaying) {
-    if (user === "ROCK" && computer === "SCISSORS") {
-      message = "You win!";
-      keepPlaying = false;
-    } else if (user === "ROCK" && computer === "PAPER") {
-      message = "You lose!";
-      keepPlaying = false;
-    } else if (user === "PAPER" && computer === "SCISSORS") {
-      message = "You lose!";
-      keepPlaying = false;
-    } else if (user === "PAPER" && computer === "ROCK") {
-      message = "You win!";
-      keepPlaying = false;
-    } else if (user === "SCISSORS" && computer === "ROCK") {
-      message = "You Lose!";
-      keepPlaying = false;
-    } else if (user === "SCISSORS" && computer === "PAPER") {
-      message = "You Win!";
-      keepPlaying = false;
-    }
+  if (user === "ROCK" && computer === "SCISSORS") {
+    message = "You win!";
+  } else if (user === "ROCK" && computer === "PAPER") {
+    message = "You lose!";
+  } else if (user === "PAPER" && computer === "SCISSORS") {
+    message = "You lose!";
+  } else if (user === "PAPER" && computer === "ROCK") {
+    message = "You win!";
+  } else if (user === "SCISSORS" && computer === "ROCK") {
+    message = "You Lose!";
+  } else if (user === "SCISSORS" && computer === "PAPER") {
+    message = "You Win!";
+  } else if (user === computer) {
+    message = `Both chose ${user}, no winner`;
   }
 
   return message;
 }
 
-userChoice = userPlay();
-console.log(userChoice);
-computerChoice = computerPlay();
+//*game Function
+function game() {
+  for (let i = 0; i < turns; i++) {
+    userChoice = userPlay();
+    computerChoice = computerPlay();
+    result = playRound(userChoice, computerChoice);
 
-let result = playRound(userChoice, computerChoice);
+    if (result === "You win!") {
+      userScore++;
+    } else {
+      computerScore++;
+    }
+  }
 
-console.log(result);
+  console.log(`ScoreBoard - User:${userScore} Computer:${computerScore}`);
+
+  if (userScore > computerScore) {
+    console.log(`You win by a score of ${userScore} to ${computerScore}`);
+  } else {
+    console.log(
+      `You lose. Computer beats you by a score of ${computerScore} to ${userScore}`
+    );
+  }
+}
+
+game();
